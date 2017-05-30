ac7216d107798d6d10fa10bced77dcae54d3f4d4
Renan Dias <renan@jera.com.br>
2017-05-29 23:01:34 -0400

    Adding comments to Lake

--- src/Lake/Lake.java
+++ src/Lake/Lake.java
@@ -9,21 +9,68 @@ import java.util.concurrent.Semaphore;
 
 /**
  * Created by yellow-umbrella on 17/05/17.
+ * Classe que define uma piscina onde passárinhos e turistas tentam acessar.
  */
 public class Lake {
 
-    public static boolean eating1 =true, eating2 = true;
+    /**
+     * Variáveis que indicam se a classe TouristEater e BirdEater ainda estão consumindo seus
+     * respectivos objetos.
+     */
+    public static boolean eating1 = true, eating2 = true;
+
+    /**
+     * Semáforo que tanca uma adição por vez de passárinho e turista.
+     */
     private Semaphore willAdd = new Semaphore(1);
 
+    /**
+     * Qauntidade máxima de turistas na piscina.
+     */
     private static int maxTourists;   // Buffer size
+
+    /**
+     * Tempo de banho de um turista.
+     */
     private static int batheTime;
+
+    /**
+     * Countador de turistas na atualmente na piscina.
+     */
     public int touristCount = 0;
+
+    /**
+     * Array de turistas na piscina.
+     */
     private Tourist[] touristArray;
+
+    /**
+     * Variávies que indicam onde deve ser retirado e onde deve ser colocado no array de turistas.
+     */
     private int in, out;
+
+    /**
+     * Semáforo que tranca modificações uma por vez nas variáveis in, out, touristCount e touristArray.
+     */
     private Semaphore mutex;
+
+    /**
+     * Semáforo que indica se pode ser adicionado turistas na piscina.
+     */
     private Semaphore empty;
+
+    /**
+     * Semáforo que indica se há turistas na piscina.
+     */
     private Semaphore full;
 
+    /**
+     * Construtor que seta variáveis necessárias para execução do programa.
+     * @param maxTourists Quantidade máxima de turistas na piscina.
+     * @param batheTime Tempo de banho de um turista.
+     * @param maxBirds Quantidade máxima de passárinhos na pisicna.
+     * @param drinkTime Tempo que passárinho leva para beber agua.
+     */
     public Lake(int maxTourists, int batheTime, int maxBirds, int drinkTime) {
         // Tourist
         in = 0;
@@ -46,6 +93,21 @@ public class Lake {
         this.drinkTime = drinkTime;
     }
 
+    /**
+     * Método que recebe um turista e tenta inserir o mesmo na piscina (Array).
+     * O semáforo "empty" indica se pode colocar um turista no Array, caso esteja cheio a Thread
+     * será travada nesse momento. Em seguida caso um passárinho não esteja sendo inserido é feito
+     * um acquire() no semáforo willAdd. A Thread entra na região critica, passando pelo mutex, e
+     * adiciona o turista na piscina (array) e calcula em que posição o proximo turista será colocado
+     * e imcrementa o contador de turistas na piscina. Uma mensagem é printada informando que o
+     * turista entrou na piscina a Thread sai da região critica.
+     * Em seguida o semáforo willAdd é liberado e a Thread é travada pelo tempo qeu o turista
+     * leva para tomar banho na piscina.
+     * Assim que esse tempo termina é feito o release no semáforo "full", indicando que há turistas
+     * a serem consumidos.
+     * @param tourist Turista tentando entrar na piscina.
+     * @throws InterruptedException
+     */
     public void insertTourist(Tourist tourist) throws InterruptedException {
         empty.acquire();
         willAdd.acquire();
@@ -62,6 +124,16 @@ public class Lake {
         full.release();
     }
 
+    /**
+     * Método que remove um turista da piscina.
+     * O semáforo full só é liberado caso haja um turista na piscina pronto para sair.
+     * Em seguida, passando pelo mutex, é retirado um turista da piscina (array) e atualizado
+     * as variáveis out e touristCount. Em seguida uma mensagem é impressa indicando que
+     * o turista saiu da piscina.
+     * Nesse momento o semáforo empty é feito o release().
+     * @return
+     * @throws InterruptedException
+     */
     public Tourist removeTourist() throws InterruptedException {
         full.acquire();
 
@@ -77,13 +149,54 @@ public class Lake {
     }
 
 
+    /**
+     * Quantidade máxima de passárinhos na piscina.
+     */
     private static int maxBirds;
+
+    /**
+     * Tempo que um passárinho leva para beber agua.
+     */
     private static int drinkTime;
+
+    /**
+     * Quantidade de passárinhos na piscina.
+     */
     public int birdCount = 0;
+
+    /**
+     * Array de passárinhos na piscina.
+     */
     private Bird[] birdsArray;
+
+    /**
+     * Variáveis que indicam onde deve adicionado o passárinho e onde deve retirar.
+     */
     private int birdIn, birdOut;
+
+    /**
+     * Semáforos para alterações de variáveis na região critica, remocçaõ e inserção de passárinhos.
+     */
     private Semaphore birdMutex, birdEmpty, birdFull;
 
+
+    /**
+     * Método para inserir um passárinho na piscina.
+     * Método que tenta inserir um passárinho na piscina. Primeiro é verificado se a piscina
+     * esta lotada de turistas, caso verdade entra em um loop onde a mensagem de que o
+     * passárinho tentou entrar mas teve que ir embora é impressa e a Thread trava pelo tempo
+     * "birdConstant". Em seguida o passárinho tenta entrar de novo na piscina. Caso a piscina
+     * não esteja lotada de turistas e não esteja lotada de passárinhos o loop é quebrado.
+     * Em seguida o semáforo "willAdd" é travado, impedindo que turistas sejam adicionados e
+     * birdEmpty também indeicando qeu será adicionado um passárinho.
+     * A Thread passa pela região critica, adicionando o turista na piscina (array) e atualizando
+     * as variáveis birdIn e virdCount.
+     * Ao sair da região critica willAdd é liberado e a mensagem de que o passárinho começou
+     * a beber é impressa. Nesse momento a Thread é travada por "walkTime" que é o tempo que
+     * passárinho leva para beber agua. Quando esse tempo acaba é chamado o release() do birdfull.
+     * @param bird Passárinho a ser adicionado na piscina.
+     * @throws InterruptedException
+     */
     public void insertBird(Bird bird) throws InterruptedException {
 
         while(touristCount == maxTourists || birdCount == maxBirds) {
@@ -111,6 +224,16 @@ public class Lake {
         birdFull.release();
     }
 
+    /**
+     * Método que remove um passárinho da piscina.
+     * É feito o acquire no semáfaro birdFull que travará caso não haja passárinhos na piscina
+     * prontos a sair. Em seguida, pela região critica, é retirado um passárinho e atualizado as
+     * váriaveis birdOut e birdCount. Uma mensagem é printada informando que o passárinho já
+     * bebeu agua e não volta mais. Nesse momento é feito um release no semáfaro birdEmpty
+     * liberando para ser adicionado novos passárinhos na piscina.
+     * @return
+     * @throws InterruptedException
+     */
     public Bird removeBird() throws InterruptedException {
         birdFull.acquire();
 
